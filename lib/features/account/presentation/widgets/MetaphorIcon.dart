173ff80ea5f96ccdfaa0638e411089210984a7d0
hdjin-desktop-Omar-3-Carlos <milkywaygod2@naver.com>
2026-05-01 07:51:19 +0900

    FEAT: MetaphorPicker 사용자 커스텀 메타포 영구 저장

--- lib/features/account/presentation/widgets/MetaphorIcon.dart
+++ lib/features/account/presentation/widgets/MetaphorIcon.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../../core/constants/Enums.dart';
 
@@ -8,10 +9,13 @@ class MetaphorIcon extends StatelessWidget {
     super.key,
     required this.nature,
     this.size = 20.0,
+    this.customEmoji,
   });
 
   final AccountNature nature;
   final double size;
+  /// null이면 nature 기본값 사용, non-null이면 커스텀 이모지 우선 표시
+  final String? customEmoji;
 
   // 자산🌳, 비용🍎, 수익💧, 부채🫙, 자본🪣
   static String emojiFor(AccountNature nature) {
@@ -24,10 +28,22 @@ class MetaphorIcon extends StatelessWidget {
     };
   }
 
+  /// accountId 기준으로 저장된 커스텀 이모지를 조회, 없으면 null 반환
+  static Future<String?> loadCustomEmoji(int accountId) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('metaphor_$accountId');
+  }
+
+  /// accountId 기준으로 커스텀 이모지를 저장
+  static Future<void> saveCustomEmoji(int accountId, String emoji) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('metaphor_$accountId', emoji);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Text(
-      emojiFor(nature),
+      customEmoji ?? emojiFor(nature),
       style: TextStyle(fontSize: size),
     );
   }
